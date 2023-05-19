5012694d9d816b94cbbb771265abd8835d7e40d3
Jon Richards <jonathan.richards@globallogic.com>
2023-05-19 08:50:35 +0100

    setup dev dependencies 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
+import { configDefaults, defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+
+  // https://vitest.dev/config/
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.js",
+    coverage: {
+      provider: "c8",
+
+      // collect coverage for all except the files specified
+      all: true,
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        "src/assets/**",
+        "src/**/types/*.ts",
+      ],
+    },
+  },
 });
