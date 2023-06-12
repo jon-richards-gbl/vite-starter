af4d747e909f5be4d302b60812059c573bc1bfe2
ellohez <helenpjobs@gmail.com>
2023-06-12 15:25:31 +0100

    UserPage buttons added and test updated to match

--- src/components/UserPage/UserForm/index.tsx
+++ src/components/UserPage/UserForm/index.tsx
@@ -7,6 +7,16 @@ export const UserForm = () => {
   const [name, setName] = useState("");
   const dispatch = useAppDispatch();
 
+  const formTitles: Array<string> = [
+    "Sign Up",
+    "Login Details",
+    "Personal Information",
+    "Address",
+  ];
+
+  // Store the current page the user is viewing
+  const [page, setPage] = useState<number>(0);
+
   const handleNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
     setName(e.target.value);
   };
@@ -18,16 +28,37 @@ export const UserForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Your name:</label>
-      <input
-        id="username"
-        name="username"
-        onInput={handleNameInput}
-        value={name}
-        required
-      />
-
-      <button type="submit">SUBMIT</button>
+      <div className="title">
+        <h1>Let's get you signed up!</h1>
+      </div>
+      <div className="row">
+        <button
+          className="form-button"
+          // Aria-disabled attibute not needed if disabled
+          // attribute included
+          disabled={page == 0}
+          tabIndex={0}
+          // No need to add Aria role of 'button' if button has type='button'
+          type="button"
+          // TODO: Set this up again
+          // onClick={() => {
+          //   setPage((currentPg) => currentPg - 1);
+          // }}
+        >
+          Previous
+        </button>
+        <button
+          className="form-button"
+          type="button"
+          disabled={page == formTitles.length - 1}
+          // TODO: Set this up again and make the button 'submit' on the final stage. Add 'submit' action?
+          // onClick={() => {
+          //   setPage((currentPg) => currentPg + 1);
+          // }}
+        >
+          Next
+        </button>
+      </div>
     </form>
   );
 };
--- src/components/UserPage/index.test.tsx
+++ src/components/UserPage/index.test.tsx
@@ -8,24 +8,32 @@ describe("User Page", () => {
     it("renders how we'd expect", () => {
       renderComponent(<UserPage />);
 
-      expect(screen.getByLabelText("Your name:")).toBeInTheDocument();
+      //expect(screen.getByLabelText("Your name:")).toBeInTheDocument();
+      let prevBtn: HTMLElement = screen.getByText("Previous");
+      expect(prevBtn).toBeInTheDocument();
+      expect(prevBtn).toBeDisabled();
+      expect(screen.getByText("Next")).toBeInTheDocument();
+      expect(screen.getByText("Next")).toEnabled();
     });
   });
 
-  describe("interact", () => {
-    it("submitting the form triggers a 'setUsername' action", () => {
-      const { store } = renderComponent(<UserPage />);
-      const actionSpy = jest.spyOn(store, "dispatch");
+  // TODO: Write to test next button.
 
-      fireEvent.input(screen.getByLabelText("Your name:"), {
-        target: { value: "Marco Polo" },
-      });
-      fireEvent.click(screen.getByText("SUBMIT"));
+  // TODO: Use this as an example to test the email entry on the first page.
+  // describe("interact", () => {
+  //   it("submitting the form triggers a 'setUsername' action", () => {
+  //     const { store } = renderComponent(<UserPage />);
+  //     const actionSpy = jest.spyOn(store, "dispatch");
 
-      expect(actionSpy).toBeCalledWith({
-        type: "user/setUserName",
-        payload: "Marco Polo",
-      });
-    });
-  });
+  //     fireEvent.input(screen.getByLabelText("Your name:"), {
+  //       target: { value: "Marco Polo" },
+  //     });
+  //     fireEvent.click(screen.getByText("SUBMIT"));
+
+  //     expect(actionSpy).toBeCalledWith({
+  //       type: "user/setUserName",
+  //       payload: "Marco Polo",
+  //     });
+  //   });
+  // });
 });
