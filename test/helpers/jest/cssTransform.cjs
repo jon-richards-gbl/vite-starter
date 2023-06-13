e88e96afc4fcc157c5fb549f365b6a1dbf246b07
ellohez <helenpjobs@gmail.com>
2023-06-13 12:43:38 +0100

    Fix was lost in reverting code

--- test/helpers/jest/cssTransform.cjs
+++ test/helpers/jest/cssTransform.cjs
@@ -5,10 +5,11 @@
 
 module.exports = {
   process() {
-    return "module.exports = {};";
+    return {
+      code: "module.exports = {};",
+    };
   },
   getCacheKey() {
-    // The output is always the same.
     return "cssTransform";
   },
 };
