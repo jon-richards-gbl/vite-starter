77b3148a0bf37fbaa524c7652848a4c51aed3e82
ellohez <helenpjobs@gmail.com>
2023-06-13 12:33:04 +0100

    Logo updated - react removed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
 import { NavLink } from "react-router-dom";
 
-import globalLogicIcon from "../../assets/global-logic-icon.png";
-import reactIcon from "../../assets/react.svg";
+//import globalLogicIcon from "../../assets/global-logic-icon.png";
+// import globalLogicIcon from '../../assets/gl-hitachi-Black-icon.svg'
+// import reactIcon from "../../assets/react.svg";
 import { PageRoutes } from "../../router";
+import Logo from "../common/Logo";
+import "./styles.css";
 
 const Header = () => {
   return (
     <header>
-      <h1>Example App</h1>
+      {/* <h1>Global Logic</h1> */}
 
-      <img src={reactIcon} alt="React Icon" />
-      <img src={globalLogicIcon} alt="GlobalLogic Icon" />
-
-      <nav>
+      {/* <img src={reactIcon} alt="React Icon" /> */}
+      <div className="svg-logo-div">
+        <Logo />
+      </div>
+      <nav className="nav-bar">
         <NavLink to={PageRoutes.LandingPage}>Home</NavLink>
-        <br />
         <NavLink to={PageRoutes.UserPage}>User</NavLink>
       </nav>
     </header>
