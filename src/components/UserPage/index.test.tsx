9a572641e31fd171acbc9461e05edc33ac50f520
ellohez <helenpjobs@gmail.com>
2023-06-13 09:56:07 +0100

    Test reverted to match old code

--- src/components/UserPage/index.test.tsx
+++ src/components/UserPage/index.test.tsx
@@ -8,32 +8,32 @@ describe("User Page", () => {
     it("renders how we'd expect", () => {
       renderComponent(<UserPage />);
 
-      //expect(screen.getByLabelText("Your name:")).toBeInTheDocument();
-      const prevBtn: HTMLElement = screen.getByText("Previous");
-      expect(prevBtn).toBeInTheDocument();
-      expect(prevBtn).toBeDisabled();
-      expect(screen.getByText("Next")).toBeInTheDocument();
-      expect(screen.getByText("Next")).toBeEnabled();
+      expect(screen.getByLabelText("Your name:")).toBeInTheDocument();
+      //const prevBtn: HTMLElement = screen.getByText("Previous");
+      //expect(prevBtn).toBeInTheDocument();
+      //expect(prevBtn).toBeDisabled();
+      //expect(screen.getByText("Next")).toBeInTheDocument();
+      //expect(screen.getByText("Next")).toBeEnabled();
     });
   });
 
   // TODO: Write to test next button.
 
   // TODO: Use this as an example to test the email entry on the first page.
-  // describe("interact", () => {
-  //   it("submitting the form triggers a 'setUsername' action", () => {
-  //     const { store } = renderComponent(<UserPage />);
-  //     const actionSpy = jest.spyOn(store, "dispatch");
+  describe("interact", () => {
+    it("submitting the form triggers a 'setUsername' action", () => {
+      const { store } = renderComponent(<UserPage />);
+      const actionSpy = jest.spyOn(store, "dispatch");
 
-  //     fireEvent.input(screen.getByLabelText("Your name:"), {
-  //       target: { value: "Marco Polo" },
-  //     });
-  //     fireEvent.click(screen.getByText("SUBMIT"));
+      fireEvent.input(screen.getByLabelText("Your name:"), {
+        target: { value: "Marco Polo" },
+      });
+      fireEvent.click(screen.getByText("SUBMIT"));
 
-  //     expect(actionSpy).toBeCalledWith({
-  //       type: "user/setUserName",
-  //       payload: "Marco Polo",
-  //     });
-  //   });
-  // });
+      expect(actionSpy).toBeCalledWith({
+        type: "user/setUserName",
+        payload: "Marco Polo",
+      });
+    });
+  });
 });
