effad05a0a0123141744cf62d3eb4afc78be8f92
ellohez <helenpjobs@gmail.com>
2023-06-14 18:20:36 +0100

    h1 - h4 examples inc for relative font sizes

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,8 +1,11 @@
 const LandingPage = () => {
   return (
     <main>
-      <div>
-        <h1>Hello world</h1>
+      <div className="main-container">
+        <h1>Hello, world! (h1)</h1>
+        <h2>These headings... (h2)</h2>
+        <h3>...are here to enable... (h3)</h3>
+        <h4>...screen reader and contrast checking (h4)</h4>
       </div>
     </main>
   );
