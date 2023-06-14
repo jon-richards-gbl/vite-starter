afa7187d324d3623f68a123202436476af9d7861
ellohez <helenpjobs@gmail.com>
2023-06-14 15:43:14 +0100

    New SignUp page component

---
+++ src/components/SignUpPage/index.tsx
@@ -0,0 +1,14 @@
+// import LogInForm from "../LogInForm";
+
+const SignUpPage = () => {
+  return (
+    <main>
+      {/* TODO: Create LogInForm and change this and import above to match */}
+      {/* <LogInForm /> */}
+
+      <h1>This will be the sign up page</h1>
+    </main>
+  );
+};
+
+export default SignUpPage;
