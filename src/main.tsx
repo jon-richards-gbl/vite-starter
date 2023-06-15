23672b923335f0405c1c108dec036bb67079b869
ellohez <helenpjobs@gmail.com>
2023-06-15 11:27:42 +0100

    Base styles set AFTER reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
+// Set up base CSS styles - must be done AFTER reset
 import "./app.css";
 import "./reset.css";
 import router from "./router";
