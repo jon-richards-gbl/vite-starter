4919f4f4377c84973469a840163aef85c0dcf882
ellohez <helenpjobs@gmail.com>
2023-06-15 14:09:59 +0100

    import moved due to conflict with reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
-// Set up base CSS styles - must be done AFTER reset
-import "./app.css";
 import "./reset.css";
 import router from "./router";
 import { store } from "./store";
