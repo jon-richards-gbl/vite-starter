00c5b6872fffc06142c2c5552b487995d38caa65
ellohez <helenpjobs@gmail.com>
2023-06-16 17:02:32 +0100

    Clean up old code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,6 @@
 import { NavLink } from "react-router-dom";
 
-//import globalLogicIcon from "../../assets/global-logic-icon.png";
 import globalLogicIcon from "../../assets/gl-hitachi-Black-icon.svg";
-// import reactIcon from "../../assets/react.svg";
 import { PageRoutes } from "../../router";
 // import Logo from "../common/Logo";
 import "./styles.css";
