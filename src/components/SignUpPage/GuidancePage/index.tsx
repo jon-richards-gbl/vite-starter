0a898dcfa8c7696c280671b913f4d0a058f11372
ellohez <helenpjobs@gmail.com>
2023-06-19 14:18:22 +0100

    First sign up page information moved to own page

---
+++ src/components/SignUpPage/GuidancePage/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./styles.css";
+
+const GuidancePage = (): JSX.Element => {
+  return (
+    <div>
+      {/* TODO: Include instructions for completing sign up - inc save option */}
+      <p id="instructions">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et distinctio
+        at mollitia numquam provident aspernatur iure, necessitatibus officia a
+        voluptates ducimus. Officiis, velit vel? Dicta ipsum ipsam quisquam
+        consequuntur natus alias! Cum accusantium pariatur similique ab earum
+        vel! Sit, accusamus!
+      </p>
+
+      <p id="terms_conditions">Terms and conditions to agree to here?</p>
+    </div>
+  );
+};
+
+export default GuidancePage;
