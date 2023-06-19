fa8ce0359bb86b34dc728f0f651d0883d56de5f8
ellohez <helenpjobs@gmail.com>
2023-06-19 17:15:44 +0100

    TS types for sign up form state

---
+++ src/types/types.tsx
@@ -0,0 +1,9 @@
+// TS type for userData
+export default interface UserData {
+  email: string;
+  password: string;
+  firstName: string;
+  surname: string;
+  birthDate: null;
+  address: string;
+}
