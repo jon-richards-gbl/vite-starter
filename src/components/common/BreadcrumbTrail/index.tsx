3a0b99c5fc5ddbe43a65027962289ea5e6ccdcfc
ellohez <helenpjobs@gmail.com>
2023-06-21 12:52:44 +0100

    Existing code moved to new component

--- src/components/common/BreadcrumbTrail/index.tsx
+++ src/components/common/BreadcrumbTrail/index.tsx
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const BreadcrumbTrail = () => {
+import "./styles.css";
+
+interface breadcrumbProps {
+  formTitles: Array<string>;
+  currentStep: number;
+}
+
+const BreadcrumbTrail: React.FC<breadcrumbProps> = ({
+  formTitles,
+  currentStep,
+}): JSX.Element => {
+  const content: Array<JSX.Element> = [];
+
+  for (let i = 0; i <= currentStep; i++) {
+    content.push(
+      <Fragment key={formTitles[i]}>
+        <div
+          className="crumb"
+          // if last element - mark as current step for aria,
+          // mark all others as false
+          aria-current={i === currentStep ? "step" : "false"}
+        >
+          <h5>Step {i + 1}</h5>
+          <p>{formTitles[i]}</p>
+        </div>
+        <div className="divider">
+          {/* Add chevrons but only BETWEEN steps */}
+          {i >= currentStep ? null : (
+            <span aria-hidden="true">
+              <i className="fa-solid fa-chevron-right"></i>
+            </span>
+          )}
+        </div>
+      </Fragment>
+    );
+  }
+
+  content.push(
+    <div className="crumb">
+      <h5>Steps Remaining</h5>
+      <p>{formTitles.length - currentStep}</p>
+    </div>
+  );
+
+  return <div className="breadcrumb-trail">{content}</div>;
   return <div></div>;
 };
 
