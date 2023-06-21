7e40fac992d9019d959005d55a5ad56f1b65b23f
ellohez <helenpjobs@gmail.com>
2023-06-21 17:05:05 +0100

    main fieldset and form tags added

--- src/components/SignUpPage/PasswordPage/index.tsx
+++ src/components/SignUpPage/PasswordPage/index.tsx
@@ -28,89 +28,95 @@ const PasswordPage: React.FC<loginDetailsProps> = ({
   // }
 
   return (
-    <div>
-      <label className="help-label" htmlFor="password" id="pwd-label">
-        Please enter a password:
-        <span className="required" aria-hidden="true">
-          Required
-        </span>
-      </label>
-      {/* Toggle between password visibility */}
-      <div className="checkbox-combo" id="password-combo">
-        <input
-          id="pwdCheckbox"
-          name="pwdCheckbox"
-          type="checkbox"
-          checked={pwdIsVisible}
-          // aria-labelledby="pwdCheckboxLabel"
-          // aria-checked={pwdIsVisible}
-          onChange={() => {
-            setPwdIsVisible((pwdIsVisible) => !pwdIsVisible);
-          }}
+    <main>
+      <form aria-labelledby="new-password">
+        <fieldset>
+          <legend id="new-password">Create new password</legend>
+          <label className="help-label" htmlFor="password" id="pwd-label">
+            Please enter a password:
+            <span className="required" aria-hidden="true">
+              Required
+            </span>
+          </label>
+          {/* Toggle between password visibility */}
+          <div className="checkbox-combo" id="password-combo">
+            <input
+              id="pwdCheckbox"
+              name="pwdCheckbox"
+              type="checkbox"
+              checked={pwdIsVisible}
+              // aria-labelledby="pwdCheckboxLabel"
+              // aria-checked={pwdIsVisible}
+              onChange={() => {
+                setPwdIsVisible((pwdIsVisible) => !pwdIsVisible);
+              }}
+            />
+            <label
+              className="checkbox-label"
+              htmlFor="pwdCheckbox"
+              id="pwdCheckboxLabel"
+            >
+              Show password?
+            </label>
+          </div>
+          <input
+            className="block-input"
+            autoFocus
+            name="password"
+            id="password"
+            autoComplete="new-password"
+            type={pwdIsVisible ? "text" : "password"}
+            aria-labelledby="pwd-label"
+            aria-describedby="pwd-desc"
+            aria-required="true"
+            value={userData.password}
+            // onBlur={blurHandler}
+            onChange={changeHandler}
+          />
+          <span className="help-label" id="pwd-desc">
+            Passwords must be between 8 and 15 characters and include uppercase
+            and lowercase letters and a number
+          </span>
+          <label
+            className="help-label"
+            htmlFor="password-confirm"
+            id="pwd-confirm-label"
+          >
+            Please re-type your new password to confirm:
+          </label>
+          <input
+            className="block-input"
+            name="password-confirm"
+            id="password-confirm"
+            type="password"
+            autoComplete="new-password"
+            aria-labelledby="pwd-confirm-label"
+            aria-required="true"
+            value={userData.passwordConfirm}
+            // onBlur={blurHandler}
+            onChange={(e) =>
+              setUserData({ ...userData, passwordConfirm: e.target.value })
+            }
+          />
+        </fieldset>
+        <PasswordChecklist
+          rules={[
+            "minLength",
+            "maxLength",
+            "specialChar",
+            "number",
+            "capital",
+            "match",
+          ]}
+          minLength={8}
+          maxLength={15}
+          value={userData.password}
+          valueAgain={userData.passwordConfirm}
+          // TODO: Enable/disable next button depending on isValid
+          onChange={(isValid) => setIsPwdValid(isValid)}
         />
-        <label
-          className="checkbox-label"
-          htmlFor="pwdCheckbox"
-          id="pwdCheckboxLabel"
-        >
-          Show password?
-        </label>
-      </div>
-      <input
-        className="block-input"
-        autoFocus
-        name="password"
-        id="password"
-        autoComplete="new-password"
-        type={pwdIsVisible ? "text" : "password"}
-        // placeholder="your password here"
-        // aria-placeholder="your password here"
-        aria-labelledby="pwd-label"
-        aria-required="true"
-        value={userData.password}
-        // onBlur={blurHandler}
-        onChange={changeHandler}
-      />
-      <PasswordChecklist
-        rules={[
-          "minLength",
-          "maxLength",
-          "specialChar",
-          "number",
-          "capital",
-          "match",
-        ]}
-        minLength={8}
-        maxLength={15}
-        value={userData.password}
-        valueAgain={userData.passwordConfirm}
-        // TODO: Enable/disable next button depending on isValid
-        onChange={(isValid) => setIsPwdValid(isValid)}
-      />
-      <label
-        className="help-label"
-        htmlFor="password-confirm"
-        id="pwd-confirm-label"
-      >
-        Please re-type your new password to confirm:
-      </label>
-      <input
-        className="block-input"
-        name="password-confirm"
-        id="password-confirm"
-        type="password"
-        autoComplete="new-password"
-        placeholder="confirm your password"
-        aria-placeholder="confirm your password"
-        aria-labelledby="pwd-confirm-label"
-        aria-required="true"
-        value={userData.passwordConfirm}
-        // onBlur={blurHandler}
-        onChange={(e) =>
-          setUserData({ ...userData, passwordConfirm: e.target.value })
-        }
-      />
-    </div>
+      </form>
+    </main>
   );
 };
 
