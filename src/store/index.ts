59040dc409481ad089c302178b98722ce9e19e65
ellohez <helenpjobs@gmail.com>
2023-06-23 15:11:24 +0100

    Comments and newUserReducer added

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,30 @@
+// This creates a Redux store, and also automatically configures the
+// Redux DevTools extension so that you can inspect the store while developing.
 import { configureStore } from "@reduxjs/toolkit";
 // this is the only place these imports are allowed. everywhere else should
 // use the app dispatch + app selector from this file
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
+import newUserReducer from "./newUser/newUserSlice";
 import userReducer from "./user/userSlice";
 
+// Add the reducer for each slice here so that they
+// are available thoughout the application
 export const createStore = (preloadedState = {}) =>
   configureStore({
     preloadedState,
     reducer: {
       user: userReducer,
+      newUser: newUserReducer,
     },
   });
 
 export const store = createStore();
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 export type AppStore = ReturnType<typeof createStore>;
 
