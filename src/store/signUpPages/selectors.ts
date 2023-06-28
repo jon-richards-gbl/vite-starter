0eda09e0d16444cbab13fc7fb1ef05b9b27f9623
ellohez <helenpjobs@gmail.com>
2023-06-28 13:06:19 +0100

    New selectors added

--- src/store/signUpPages/selectors.ts
+++ src/store/signUpPages/selectors.ts
@@ -4,9 +4,27 @@ import { RootState } from "../index";
 
 const signUpPageState = (state: RootState) => state.signUpPages;
 
-export const selectPageWithIndex = createSelector(
+export const selectPageByIndex = createSelector(
   [signUpPageState, (signUpPageState, index: number) => index],
   (signUpPageState, index) => {
     return signUpPageState.pages[index];
   }
+  // [
+  //   signUpPageState => signUpPageState.pages,
+  //   (signUpPageState, index: number) => index
+  // ],
+  // (pages, index) => pages.filter(index)
+);
+
+export const selectLastPage = createSelector(
+  [signUpPageState],
+  (newPageState) => {
+    const lastIndex = newPageState.pages.length - 1;
+    return newPageState.pages[lastIndex];
+  }
+);
+
+export const selectNumPages = createSelector(
+  [signUpPageState],
+  (newPageState) => newPageState.pages.length
 );
