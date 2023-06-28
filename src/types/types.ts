416df4fab45a99d7071792cf09f65171201d2567
ellohez <helenpjobs@gmail.com>
2023-06-28 15:59:41 +0100

    UserData now in Redux

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,10 @@
 // TS type for userData
-export default interface UserData {
-  email: string;
-  password: string;
-  passwordConfirm: string;
-  firstName: string;
-  surname: string;
-  birthDate: null;
-  address: string;
-}
+// export default interface UserData {
+//   email: string;
+//   password: string;
+//   passwordConfirm: string;
+//   firstName: string;
+//   surname: string;
+//   birthDate: null;
+//   address: string;
+// }
