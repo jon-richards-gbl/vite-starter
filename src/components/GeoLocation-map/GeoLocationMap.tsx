b10e2b6d0bfeaf1c5f4519f8c3500d7d0caa6355
jimbobding <jimmygrahamdavid@gmail.com>
2023-06-29 14:22:48 +0100

    added places search for breweries in radius to the user'

--- src/components/GeoLocation-map/GeoLocationMap.tsx
+++ src/components/GeoLocation-map/GeoLocationMap.tsx
@@ -1,22 +1,19 @@
 import { GoogleMap } from "@react-google-maps/api";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
-type LatLngLiteral = google.maps.LatLngLiteral;
+// ... Import statements
 
+type LatLngLiteral = google.maps.LatLngLiteral;
 type MapOptions = google.maps.MapOptions;
 
 const GeoLocationMap = () => {
   const mapRef = useRef<google.maps.Map | null>(null);
-  // const center = useMemo<LatLngLiteral>(
-  //   () => ({ lat: 51.5072, lng: 0.1276 }),
-  //   []
-  // );
   const [center, setCenter] = useState<LatLngLiteral>({
     lat: 51.5072,
     lng: 0.1276,
   });
+  const [loading, setLoading] = useState(true);
 
-  // Map options // Use memo so options do not render unless changed
   const options = useMemo<MapOptions>(
     () => ({
       mapId: "55ec9d32771d5e8c",
@@ -32,19 +29,82 @@ const GeoLocationMap = () => {
         (position) => {
           const { latitude, longitude } = position.coords;
           setCenter({ lat: latitude, lng: longitude });
+          setLoading(false); // Set loading to false once geolocation is fetched
         },
         (error) => {
           console.error("Error getting geolocation:", error);
+          setLoading(false); // Set loading to false even if geolocation fails
         }
       );
     } else {
       console.error("Geolocation is not supported by this browser.");
+      setLoading(false); // Set loading to false if geolocation is not supported
     }
   }, []);
 
-  const onLoad = useCallback((map: google.maps.Map) => {
-    mapRef.current = map;
-  }, []);
+  const onLoad = useCallback(
+    (map: google.maps.Map) => {
+      mapRef.current = map;
+
+      // Only execute the rest of the code if geolocation has been fetched
+      if (!loading) {
+        const service = new google.maps.places.PlacesService(map);
+        const infowindow = new google.maps.InfoWindow();
+
+        const createMarker = (place: google.maps.places.PlaceResult) => {
+          if (!place.geometry || !place.geometry.location) return;
+
+          const marker = new google.maps.Marker({
+            map,
+            position: place.geometry.location,
+          });
+
+          google.maps.event.addListener(marker, "click", () => {
+            infowindow.setContent(place.name || "");
+            infowindow.open(map, marker);
+          });
+        };
+
+        const request = {
+          location: map.getCenter(),
+          radius: 5000,
+          query: "brewery",
+        };
+
+        service.textSearch(
+          request,
+          (
+            results: google.maps.places.PlaceResult[] | null,
+            status: google.maps.places.PlacesServiceStatus
+          ) => {
+            if (
+              status === google.maps.places.PlacesServiceStatus.OK &&
+              results
+            ) {
+              for (let i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+              }
+
+              const firstResult = results[0];
+              if (
+                firstResult &&
+                firstResult.geometry &&
+                firstResult.geometry.location
+              ) {
+                map.panTo(firstResult.geometry.location);
+              }
+            }
+          }
+        );
+      }
+    },
+    [loading]
+  );
+
+  if (loading) {
+    return <div>Loading...</div>; // Display a loading indicator while fetching geolocation
+  }
+
   return (
     <GoogleMap
       zoom={14}
@@ -52,7 +112,7 @@ const GeoLocationMap = () => {
       mapContainerClassName="map-container1"
       options={options}
       onLoad={onLoad}
-    ></GoogleMap>
+    />
   );
 };
 
