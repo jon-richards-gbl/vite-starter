cf0251a058b311d90df816b6a936854e8c5f2c36
jimbobding <jimmygrahamdavid@gmail.com>
2023-06-29 13:13:27 +0100

    added geolocation service to geolocation file

--- src/components/GeoLocation-map/GeoLocationMap.tsx
+++ src/components/GeoLocation-map/GeoLocationMap.tsx
@@ -1,16 +1,20 @@
 import { GoogleMap } from "@react-google-maps/api";
-import { useCallback, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 type LatLngLiteral = google.maps.LatLngLiteral;
-type DirectionsResult = google.maps.DirectionsResult;
+
 type MapOptions = google.maps.MapOptions;
 
 const GeoLocationMap = () => {
   const mapRef = useRef<google.maps.Map | null>(null);
-  const center = useMemo<LatLngLiteral>(
-    () => ({ lat: 53.483959, lng: -2.244644 }),
-    []
-  );
+  // const center = useMemo<LatLngLiteral>(
+  //   () => ({ lat: 51.5072, lng: 0.1276 }),
+  //   []
+  // );
+  const [center, setCenter] = useState<LatLngLiteral>({
+    lat: 51.5072,
+    lng: 0.1276,
+  });
 
   // Map options // Use memo so options do not render unless changed
   const options = useMemo<MapOptions>(
@@ -22,12 +26,28 @@ const GeoLocationMap = () => {
     []
   );
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCenter({ lat: latitude, lng: longitude });
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   const onLoad = useCallback((map: google.maps.Map) => {
     mapRef.current = map;
   }, []);
   return (
     <GoogleMap
-      zoom={10}
+      zoom={14}
       center={center}
       mapContainerClassName="map-container1"
       options={options}
