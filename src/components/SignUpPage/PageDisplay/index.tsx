3c79dc2b59d7cc2c1b601cba49f7db3efcf55e11
ellohez <helenpjobs@gmail.com>
2023-07-03 17:47:27 +0100

    Safer page state creation and fix for page 0

--- src/components/SignUpPage/PageDisplay/index.tsx
+++ src/components/SignUpPage/PageDisplay/index.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 
+import { formTitles } from "..";
 import { useAppDispatch, useAppSelector } from "../../../store";
 import { selectNumPages } from "../../../store/signUpPages/selectors";
-import { addPage } from "../../../store/signUpPages/signUpPagesSlice";
-import { SignUpPageInformation } from "../../../store/signUpPages/state";
+import {
+  /* addPage */
+  createPage,
+} from "../../../store/signUpPages/signUpPagesSlice";
+// import { SignUpPageInformation } from "../../../store/signUpPages/state";
 import EmailPage from "../EmailPage";
 import GuidancePage from "../GuidancePage";
 import PasswordPage from "../PasswordPage";
@@ -19,19 +23,20 @@ const PageDisplay = ({ page }: { page: number }): React.JSX.Element => {
     // to the state. Passing the index to the child component.
     // Ensure that there is always a state object for each page.
 
-    if (numPages < page) {
-      const newPage: SignUpPageInformation = {
-        index: page,
-        isValid: false,
-        errorMessages: [],
-      };
-      dispatch(addPage(newPage));
+    if (numPages < page || numPages === 0) {
+      // const newPage: SignUpPageInformation = {
+      //   index: page,
+      //   isValid: false,
+      //   errorMessages: [],
+      // };
+      // dispatch(addPage(newPage));
+      dispatch(createPage(formTitles[page]));
     }
   }, [dispatch, numPages, page]);
 
   switch (page) {
     case 0:
-      return <GuidancePage index={page} />;
+      return <GuidancePage id={formTitles[page]} />;
     case 1:
       return <EmailPage />;
     case 2:
