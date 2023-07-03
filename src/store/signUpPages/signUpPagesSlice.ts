53811c92ef8d2a64f75ea2fcb20f9625e6c6531b
ellohez <helenpjobs@gmail.com>
2023-07-03 17:42:04 +0100

    Improvements to reducers

--- src/store/signUpPages/signUpPagesSlice.ts
+++ src/store/signUpPages/signUpPagesSlice.ts
@@ -9,20 +9,47 @@ const signUpPageSlice = createSlice({
   name: "signUpPages",
   initialState: createInitialSignUpPagesState,
   reducers: {
-    addPage(state, action: PayloadAction<SignUpPageInformation>): void {
-      const newPage: SignUpPageInformation = action.payload;
-      newPage.index = state.pages.length;
-      state.pages.push(newPage);
+    // addPage(state, action: PayloadAction<SignUpPageInformation>): void {
+    //   const newPage: SignUpPageInformation = action.payload;
+    //   newPage.index = state.pages.length;
+    //   state.pages.push(newPage);
+    // },
+    createPage(state, action: PayloadAction<string>): void {
+      // Ensure page does not already exist
+      const page = state.pages.find((page) => page.id === action.payload);
+      if (page === undefined || page === null) {
+        const newPage: SignUpPageInformation = {
+          id: action.payload,
+          isValid: false,
+          errorMessages: [],
+        };
+
+        state.pages.push(newPage);
+      }
     },
-    updatePage(state, action: PayloadAction<SignUpPageInformation>): void {
-      state.pages[action.payload.index] = action.payload;
+    setValidTrue(state, action: PayloadAction<string>): void {
+      const pageFound = state.pages.find((page) => page.id === action.payload);
+      if (pageFound) {
+        pageFound.isValid = true;
+      }
     },
+    setValidFalse(state, action: PayloadAction<string>): void {
+      const page = state.pages.find((page) => page.id === action.payload);
+      if (page) {
+        page.isValid = false;
+      }
+    },
+
+    // updatePage(state, action: PayloadAction<SignUpPageInformation>): void {
+    //   state.pages[action.payload.index] = action.payload;
+    // },
     resetPages(state) {
       state.pages = new Array<SignUpPageInformation>();
     },
   },
 });
 
-export const { addPage, updatePage, resetPages } = signUpPageSlice.actions;
+export const { createPage, setValidTrue, setValidFalse, resetPages } =
+  signUpPageSlice.actions;
 
 export default signUpPageSlice.reducer;
