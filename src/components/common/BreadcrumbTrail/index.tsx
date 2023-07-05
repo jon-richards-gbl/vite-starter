f8e480df28b09eb062c0d8433071db94803eebd1
ellohez <helenpjobs@gmail.com>
2023-07-05 16:01:38 +0100

    Tidy up

--- src/components/common/BreadcrumbTrail/index.tsx
+++ src/components/common/BreadcrumbTrail/index.tsx
@@ -3,6 +3,7 @@ import React, { Fragment } from "react";
 import { formTitles } from "../../SignUpPage";
 import "./styles.css";
 
+// TODO: Clear this? Should titles be passed in rather than imported?
 // interface breadcrumbProps {
 //   formTitles: Array<string>;
 //   currentStep: number;
