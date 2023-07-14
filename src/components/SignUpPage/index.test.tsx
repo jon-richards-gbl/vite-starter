0a01b6849305d2fd8e3621b22911e943a6499e2c
ellohez <helenpjobs@gmail.com>
2023-07-14 11:18:17 +0100

    Todo spec tests added

---
+++ src/components/SignUpPage/index.test.tsx
@@ -0,0 +1,23 @@
+// import { screen } from "@testing-library/react";
+// import SignUpPage from ".";
+// import { renderComponent } from "../../../test/helpers/render";
+export {};
+
+describe("Sign Up Page", () => {
+  describe("Render", () => {
+    // it("renders how we expect", () => {
+    //     renderComponent(<SignUpPage />);
+
+    //     expect(screen.getByTitle("Sign up for our services")).toBeInTheDocument();
+    //     expect(screen.getByTitle("Sign Up - Guidance")).toBeInTheDocument();
+    // });
+    it.todo("Previous button is enabled on initial render and middle pages");
+    it.todo("Previous button is disabled when last page is reached");
+    it.todo("Next button is not enabled");
+  });
+});
+//const prevBtn: HTMLElement = screen.getByText("Previous");
+//expect(prevBtn).toBeInTheDocument();
+//expect(prevBtn).toBeDisabled();
+//expect(screen.getByText("Next")).toBeInTheDocument();
+//expect(screen.getByText("Next")).toBeEnabled();
